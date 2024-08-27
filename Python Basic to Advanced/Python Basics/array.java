9a9afa52d21152974c6d91c2ce4605d4949332c4
akash09gupta <akashgupta211ag@gmail.com>
2024-08-27 11:16:19 +0530

    java file added

---
+++ Python Basic to Advanced/Python Basics/array.java
@@ -0,0 +1,14 @@
+class Array {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 10;
+        arr[1] = 15;
+        arr[2] = 16;
+        arr[3] = 17;
+        arr[4] = 18;
+
+        for(int i=0; i<5; i++) {
+            System.out.println(arr[i]);
+        }
+        }
+}
